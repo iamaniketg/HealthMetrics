4d7403f35aa81699419a073606e50dbf76eaf623
mianiket0612 <ANIKET.GOSAVI@softnerve.com>
2024-12-24 18:43:38 +0530

    Added First Service

---
+++ User-Microservice/src/test/java/com/Health_Metrics/User_Microservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Health_Metrics.User_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
